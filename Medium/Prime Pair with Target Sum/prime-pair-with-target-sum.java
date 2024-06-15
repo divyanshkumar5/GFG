3abaab1eed2fbcca3b2bff1955d74d0b39d83c35
Divyansh <94822471+divyanshkumar5@users.noreply.github.com>
2024-06-16 00:28:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Prime Pair with Target Sum/prime-pair-with-target-sum.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            Solution obj = new Solution();
+            ArrayList<Integer> res = obj.getPrimes(n);
+
+            IntArray.print(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    private static boolean checkPrime(int x) {
+        if (x <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<Integer> getPrimes(int n) {
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int i = 2; i <= n / 2; i++) {
+            if (checkPrime(i) && checkPrime(n - i)) {
+                res.add(i);
+                res.add(n - i);
+                return res;
+            }
+        }
+        res.add(-1);
+        res.add(-1);
+        return res;
+    }
+
+}
