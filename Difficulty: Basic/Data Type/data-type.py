6890cadff4faa88e49f2056177fcbf54fabddb1d
Divyansh <94822471+divyanshkumar5@users.noreply.github.com>
2024-08-10 12:44:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Data Type/data-type.py
@@ -0,0 +1,28 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def dataTypeSize(self, str:str) -> int:
+        # Code here
+        dir = {"character":1 , "integer": 4, "short": 2, "long": 8, "float": 4,
+        "double": 8}
+        strnew = str.lower()
+        if strnew in dir.keys():
+            return dir[strnew]
+        else :
+            return -1
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        str = input()
+        ob = Solution()
+        res = ob.dataTypeSize(str)
+        print(res)
+# } Driver Code Ends
